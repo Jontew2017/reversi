51e3c8578b53f50c891fe49abe6a52449fef03db
Jonte Williams <jontew@uci.edu>
2017-06-09 22:56:19 -0700

    More updates I don’t like

--- public/js/main.js
+++ public/js/main.js
@@ -82,10 +82,10 @@ dom_elements.slideDown(1000);
 }
 
 /* Manage the message that a new player has joined */
-var newHTML = '<p>'+payload.username+' just entered the lobby</p>';
+var newHTML = '<p>'+payload.username+' just entered the room</p>';
 var newNode = $(newHTML);
 newNode.hide();
-$('#messages').append(newNode);
+$('#messages').prepend(newNode);
 newNode.slideDown(1000);
 });
 
@@ -109,10 +109,10 @@ if(dom_elements.length != 0) {
 }
 
 /* Manage the message that a player has left */
-var newHTML = '<p>'+payload.username+' has left the lobby</p>';
+var newHTML = '<p>'+payload.username+' has left the room</p>';
 var newNode = $(newHTML);
 newNode.hide();
-$('#messages').append(newNode);
+$('#messages').prepend(newNode);
 newNode.slideDown(1000);
 });
 
@@ -208,6 +208,7 @@ function send_message(){
   payload.message = $('#send_message_holder').val();
   console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
   socket.emit('send_message',payload);
+  $('#send_message_holder').val('');
 }
 
 
@@ -219,7 +220,7 @@ socket.on('send_message_response',function(payload) {
 var newHTML = '<p><b>'+payload.username+' says:</b> '+payload.message+'</p>';
 var newNode = $(newHTML);
 newNode.hide();
-$('#messages').append(newNode);
+$('#messages').prepend(newNode);
 newNode.slideDown(1000);
 });
 
@@ -275,6 +276,8 @@ var old_board = [
   ['?','?','?','?','?','?','?','?']
 ];
 
+var my_color = ' ';
+
 socket.on('game_update',function(payload) {
  console.log('*** Client Log Message: \'game_update\'\n\tpayload: '+JSON.stringify(payload));
  /* Check for a good board update */
@@ -287,12 +290,26 @@ socket.on('game_update',function(payload) {
 /* check for a good board  in the payload */
 
 var board = payload.game.board;
-if('undefined' == typeof !board || !board) {
+if('undefined' == typeof board || !board) {
   console.log('Internal error: received a malformed board update from the server');
   return;
 }
 
 /* Update my color */
+if(socket.id == payload.game.player_white.socket) {
+  my_color = 'white';
+}
+  else if(socket.id == payload.game.player_black.socket){
+    my_color = 'black';
+}
+  else{
+  /* Something weird is going on, like three people playing at once */
+  /* send client back to the lobby */
+  window.location.href = 'lobby.html?username='+username;
+  return;
+}
+
+$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
 
 /* Animate changes to the board */
 
@@ -301,7 +318,7 @@ for(row = 0; row < 8; row++) {
 for(column = 0; column < 8; column++) {
   /* If a board space has changed */
   if(old_board[row][column] != board [row][column]) {
-    if(old_board[row][column] == '?' && board[row][column] == '') {
+    if(old_board[row][column] == '?' && board[row][column] == ' ') {
       $('#'+row+'_'+column).html('<img src="assets/images/empty.gif" alt="empty square"/>');
     }
     else if(old_board[row][column] == '?' && board[row][column] == 'w') {
@@ -316,10 +333,10 @@ for(column = 0; column < 8; column++) {
     else if(old_board[row][column] == ' ' && board[row][column] == 'b') {
       $('#'+row+'_'+column).html('<img src="assets/images/empty_to_black.gif" alt="black square"/>');
     }
-    else if(old_board[row][column] == 'w' && board[row][column] == '') {
+    else if(old_board[row][column] == 'w' && board[row][column] == ' ') {
       $('#'+row+'_'+column).html('<img src="assets/images/blue_to_empty.gif" alt="empty square"/>');
     }
-    else if(old_board[row][column] == 'b' && board[row][column] == '') {
+    else if(old_board[row][column] == 'b' && board[row][column] == ' ') {
       $('#'+row+'_'+column).html('<img src="assets/images/black_to_empty.gif" alt="empty square"/>');
     }
     else if(old_board[row][column] == 'w' && board[row][column] == 'b') {
@@ -331,8 +348,37 @@ for(column = 0; column < 8; column++) {
     else{
       $('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error square"/>');
     }
+
+    /* Set up interactivity */
+    $('#'+row+'_'+column).off('click');
+    if(board[row][column] == ''){
+      $('#'+row+'_'+column).addClass('hovered_over');
+      $('#'+row+'_'+column).click(function(r,c){
+        return function(){
+          var payload = {};
+          payload.row = r;
+          payload.column = c;
+          payload.color = my_color;
+          console.log('*** Client Log Message: \'play_token\' payload: '+JSON.stringify(payload));
+          socket.emit('play_token',payload);
+        };
+      }(row,column));
+    }
+    else {
+      $('#'+row+'_'+column).removeClass('hovered_over');
+    }
   }
-}
+  }
 }
 old_board = board;
 });
+
+socket.on('play_token_response',function(payload) {
+ console.log('*** Client Log Message: \'game_token_response\'\n\tpayload: '+JSON.stringify(payload));
+ /* Check for a good play_token_response */
+ if(payload.result == 'fail') {
+   console.log(payload.message);
+   alert(payload.message);
+   return;
+ }
+ });
