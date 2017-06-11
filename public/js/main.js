2701ffbfacce0d80556247d706c949566afb6e83
Jonte Williams <jontew@uci.edu>
2017-06-10 19:32:24 -0700

    Great updates

--- public/js/main.js
+++ public/js/main.js
@@ -263,6 +263,9 @@ $(function() {
   payload.username = username;
   console.log('*** Client Log Message: \'join_room\' payload: '+JSON.stringify(payload));
   socket.emit('join_room',payload);
+
+  $('#quit').append('<a href="lobby.html?username='+username+'" class="btn btn-danger btn-default active" role="button" aria-pressed="true">Quit</a>');
+
 });
 
 var old_board = [
@@ -313,9 +316,18 @@ $('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
 
 /* Animate changes to the board */
 
+
+var blacksum = 0;
+var whitesum = 0;
 var row,column;
 for(row = 0; row < 8; row++) {
 for(column = 0; column < 8; column++) {
+if(board[row][column] == 'b') {
+  blacksum++;
+}
+if(board[row][column] == 'w') {
+  whitesum++;
+}
   /* If a board space has changed */
   if(old_board[row][column] != board [row][column]) {
     if(old_board[row][column] == '?' && board[row][column] == ' ') {
@@ -351,7 +363,7 @@ for(column = 0; column < 8; column++) {
 
     /* Set up interactivity */
     $('#'+row+'_'+column).off('click');
-    if(board[row][column] == ''){
+    if(board[row][column] == ' '){
       $('#'+row+'_'+column).addClass('hovered_over');
       $('#'+row+'_'+column).click(function(r,c){
         return function(){
@@ -370,6 +382,9 @@ for(column = 0; column < 8; column++) {
   }
   }
 }
+$('#blacksum').html(blacksum)
+$('#whitesum').html(whitesum)
+
 old_board = board;
 });
 
@@ -382,3 +397,15 @@ socket.on('play_token_response',function(payload) {
    return;
  }
  });
+
+ socket.on('game_over',function(payload) {
+  console.log('*** Client Log Message: \'game_over\'\n\tpayload: '+JSON.stringify(payload));
+  /* Check for a good play_token_response */
+  if(payload.result == 'fail') {
+    console.log(payload.message);
+    return;
+  }
+  /* Jump to a new page */
+  $('#game_over').html('<h1>Game Over</h1><h2>'+payload.who_won+' won!</h2>');
+  $('#game_over').append('<a href="lobby.html?username='+username+'" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Return to the lobby</a>');
+  });
--- server.js
+++ server.js
@@ -205,7 +205,7 @@ socket.on('send_message',function(payload) {
                                            });
         return;
       }
-      var username = payload.username;
+      var username = players[socket.id].username;
       if(('undefined' === typeof username) || !username){
         var error_message = 'send_message didn\'t specify a username, command aborted';
         log(error_message);
@@ -217,7 +217,7 @@ socket.on('send_message',function(payload) {
         }
         var message = payload.message;
         if(('undefined' === typeof message) || !message){
-          var error_message = 'send_message didn\'t specify a username, command aborted';
+          var error_message = 'send_message didn\'t specify a message, command aborted';
           log(error_message);
             socket.emit('send_message_response',   {
                                                  result: 'fail',
@@ -732,4 +732,32 @@ socket.on('send_message',function(payload) {
         };
         io.in(game_id).emit('game_update',success_data);
         /* Check to see if the game is over */
-      }
+
+        var row,column;
+        var count = 0;
+        for(row = 0; row < 8;row++) {
+        for(column = 0; column < 8;column++) {
+          if(games[game_id].board[row][column] != ' '){
+            count++;
+          }
+       }
+    }
+
+    if(count == 64) {
+      /* Send a game over message */
+      var success_data = {
+        result:'success',
+        game: games[game_id],
+        who_won: 'everyone',
+        game_id: game_id
+      };
+      io.in(game_id).emit('game_over', success_data);
+
+      /* Delete old games after 1 hour */
+      setTimeout(function(id){
+        return function(){
+          delete games[id];
+        }}(game_id)
+        ,60*60*1000);
+    }
+  }
